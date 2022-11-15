0f3be50f284d695dc8e1f7a9953ad9b81887c08b
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-15 15:37:58 +0200

    second commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,9 +3,15 @@ import org.junit.Test;
 
 public class MainTest extends Main{
     @Test
-    public void doFactorialTest() {
+    public void doFirstFactorialTest() {
         Main main = new Main();
         int result = doFactorial(5);
         Assert.assertEquals(result, 120);
+    }
+    @Test
+    public void doSecondFactorialTest() {
+        Main main = new Main();
+        int result = doFactorial(4);
+        Assert.assertEquals(result, 123);
     }
 }
